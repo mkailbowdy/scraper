f7b29df8f3cab6a6ef6b86678b21e429718512a4
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2025-05-30 05:34:38 +0900

    change

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,19 +1,17 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/gocolly/colly"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 // Define a handler
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-
-	//shigotos, err := app.shigotos.Latest()
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//	return
-	//}
 
 	files := []string{
 		"./ui/html/base.tmpl.html",
@@ -30,11 +28,75 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data := templateData{
 		//Shigotos: shigotos,
 	}
+	count := 8
+	flag.Parse()
+	// Instantiate default collector
+	var products []byte
+
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("www.goodsmile.com"),
+	)
+
+	var ranOnce bool
+	c.OnHTML("div.c-top-product-list__unit", func(e *colly.HTMLElement) {
+		if ranOnce {
+			return
+		}
+		ranOnce = true
+
+		e.ForEachWithBreak("a.c-top-product-list__item[href]", func(i int, h *colly.HTMLElement) bool {
+			fmt.Println(i, count)
+			if i >= count {
+				return false
+			}
+			link := h.Attr("href")
+			c.Visit(h.Request.AbsoluteURL(link))
+			return true
+		})
+	})
+
+	getDetails(c, "h1.b-product-info__title", &products)
+	getDetails(c, "span.c-price__main", &products)
+	getDetails(c, "p.b-product-info__note", &products)
+	getDetails(c, "p[name]", &products)
+
+	c.OnHTML("section.p-product__section a", func(e *colly.HTMLElement) {
+		if e.Text == "パートナーショップ一覧" {
+			fmt.Println("Parter? Yes!", e.Attr("href"))
+		} else {
+			fmt.Println("Partner? No!")
+		}
+	})
+
+	c.OnHTML("div#specification", func(e *colly.HTMLElement) {
+		e.ForEach("dl.b-outline-table__detail", func(_ int, dl *colly.HTMLElement) {
+			term := dl.ChildText("dt h3")
+			if term == "仕様" || term == "Specifications" {
+				specText := dl.ChildText("dd p")
+				products = append(products, []byte(specText+"\n"+"============================================\n\n")...)
+				fmt.Printf("Specification:%s\n\n\n", specText)
+				fmt.Print("============================================")
+			}
+		})
+	})
+
+	c.Visit("https://www.goodsmile.com/ja")
+	c.Visit("https://www.goodsmile.com/en")
+	os.WriteFile("/ui/static/goodsmile.txt", products, 0644)
 
 	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
+
+}
+func getDetails(c *colly.Collector, goquerySelector string, product *[]byte) {
+	c.OnHTML(goquerySelector, func(e *colly.HTMLElement) {
+		detail := e.DOM.Text()
+		fmt.Printf("%s\n", detail)
+		*product = append(*product, []byte(detail+"\n")...)
+	})
 }
 
 //func (app *application) shigotoView(w http.ResponseWriter, r *http.Request) {
